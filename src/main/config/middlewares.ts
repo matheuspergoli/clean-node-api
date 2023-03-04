acc16da265818dcc14ddb71a6bf4ac6b779869fc
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2023-03-04 20:05:01 -0300

    feat: ensure cors is enabled

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import type { Express } from 'express'
+import { cors } from '../middlewares/cors'
 import { bodyParser } from '../middlewares/body-parser'
 
 export default (app: Express): void => {
+  app.use(cors)
   app.use(bodyParser)
 }
